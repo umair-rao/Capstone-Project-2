0851d5a9fcad0b47d6719044421e8f950010e2d2
umair-rao <umairmuqeem@gmail.com>
2022-11-30 15:08:33 +0500

    Fetch API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css'
+import displayMovie from './modules/displayMovie.js';
+
+const url = 'https://api.tvmaze.com/shows';
+
+const getMovie = async () => {
+  const images = fetch(url,
+    { method: 'GET' })
+    .then((response) => response.json())
+    .then((data) => {
+      // const movieList = data;
+      displayMovie(data);
+    });
+};
+
+getMovie();
