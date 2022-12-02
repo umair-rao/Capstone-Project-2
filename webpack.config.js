14b3543ca574c622df4b98d8bcf3a65342a6dad9
umair-rao <umairmuqeem@gmail.com>
2022-12-02 23:31:03 +0500

    develop merge

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  devtool: 'inline-source-map',
+  entry: './src/index.js',
   devServer: {
     static: './dist',
   },
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
